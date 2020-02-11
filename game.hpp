209c9e3b00abed290d8b34b09e635bb89c0ed223
Shani Rub <43911148+shanirub@users.noreply.github.com>
2020-02-11 16:01:04 +0100

    added doc

--- game.hpp
+++ game.hpp
@@ -3,27 +3,27 @@
 class game
 {
     private:
-        int MAX_NMISS = 10;
-        char * VALID_GUESS = "abcdefghijklmnopqrstuvwxyz";
-        char guess;
-        int nguess;
-        int nmiss;
-        char * wordtosolve;
-        char * wordsofar;
-        char * oldguess;
+        int MAX_NMISS = 10; // max. number of misses allowed
+        char * VALID_GUESS = "abcdefghijklmnopqrstuvwxyz"; // a string containing the allowed guesses (all the letter)
+        char guess; // the new guess , to be read from user every round
+        int nguess; // number of guesses made so far
+        int nmiss; // number of misses so far
+        char * wordtosolve; // the original word
+        char * wordsofar; // the word as shown during the game. each letter is represented as "-" until correctly guessed
+        char * oldguess; // a string containing the former guesses (in lower case)
 
-        bool isguessinword(char guess, char * wordtosolve);
-        bool isnewguess(char guess);
-        bool issolved(char * wordsofar);
-        bool ismaxguess(int nguess);
-        char readguess();
-        bool isvalidguess(char guess);
+        bool isguessinword(char guess, char * wordtosolve); // returns true if the guess is correct
+        bool isnewguess(char guess); // returns true if the guess was not guessed before (according to oldguess)
+        bool issolved(char * wordsofar); // returns true when the entire word was guessed. game ends - win
+        bool ismaxguess(int nguess); // returns true when max. number of misses was reached. game ends - loss
+        char readguess(); // reads the guess, and returns it
+        bool isvalidguess(char guess); // returns true if the guess is a letter (upper or lower case)
 
 
     public:
-        game();
-        game(char * wordtoguess);
-        ~game();
-        void startgame();
+        game(); // default constructor - should call the other constructor with a default word???
+        game(char * wordtoguess); // constructor with a word to guess
+        ~game(); // deconstructor - what should it do???
+        void startgame(); // the function that begins a new game. this is the first function to be called from main.cpp
 
 };
