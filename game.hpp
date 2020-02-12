994ed26a49887d1ac28158e3537599f94e7f5a19
Shani Rub <43911148+shanirub@users.noreply.github.com>
2020-02-12 12:10:41 +0100

    header

--- game.hpp
+++ game.hpp
@@ -12,18 +12,19 @@ class game
         char * wordsofar; // the word as shown during the game. each letter is represented as "-" until correctly guessed
         char * oldguess; // a string containing the former guesses (in lower case)
 
+        
+
+
+    public:
+        game(); // default constructor - should call the other constructor with a default word???
+        game(char * wordtosolve); // constructor with a word to solve
+        ~game(); // deconstructor - what should it do???
+  
         bool isguessinword(char guess, char * wordtosolve); // returns true if the guess is correct
         bool isnewguess(char guess); // returns true if the guess was not guessed before (according to oldguess)
         bool issolved(char * wordsofar); // returns true when the entire word was guessed. game ends - win
         bool ismaxguess(int nguess); // returns true when max. number of misses was reached. game ends - loss
         char readguess(); // reads the guess, and returns it
         bool isvalidguess(char guess); // returns true if the guess is a letter (upper or lower case)
-
-
-    public:
-        game(); // default constructor - should call the other constructor with a default word???
-        game(char * wordtoguess); // constructor with a word to guess
-        ~game(); // deconstructor - what should it do???
-        void startgame(); // the function that begins a new game. this is the first function to be called from main.cpp
 
 };
